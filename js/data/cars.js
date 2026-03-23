62ff6d5dccc6286ad212c82438b8073c5805bf11
AlexanderGo23 <biggum@yandex.ru>
2026-03-23 09:30:10 +0300

    1

---
+++ js/data/cars.js
@@ -0,0 +1,15 @@
+export const garages = {
+    normal: [
+        { id: "volga", name: "Внедорожник Волга Т50", img: "volga.png", tank: 80, cons: 12, hpLoss: 0.3, sleepBonus: 0, radius: 40 },
+        { id: "moskvich", name: "Седан Москвич 6", img: "moskvich.png", tank: 50, cons: 8, hpLoss: 0.8, sleepBonus: 0, radius: 15 },
+        { id: "gazel", name: "Автодом Газ Некст", img: "gazel.png", tank: 70, cons: 15, hpLoss: 0.5, sleepBonus: 100, radius: 20 }
+    ],
+    hard: [
+        { id: "uaz", name: "УАЗ Патриот", img: "uaz.png", tank: 75, cons: 16, hpLoss: 1.0, sleepBonus: 0, radius: 40 },
+        { id: "granta", name: "Лада Гранта", img: "granta.png", tank: 45, cons: 9, hpLoss: 1.5, sleepBonus: 0, radius: 15 },
+        { id: "lada_camper", name: "Автокемпер Лада", img: "lada_camper.png", tank: 50, cons: 11, hpLoss: 1.2, sleepBonus: 60, radius: 20 }
+    ],
+    ultra: [
+        { id: "bike", name: "🚲 Велосипед", img: "bike.png", tank: 0, cons: 0, hpLoss: 0.1, sleepBonus: 0, radius: 5 }
+    ]
+};
---
+++ js/data/prices.js
@@ -0,0 +1,5 @@
+export const prices = {
+    1: { hotel: 400, hostel: 250, rest: 300, fastfood: 150, repair: 10, gas: 7, exc: 200, quizReward: 150 },
+    2: { hotel: 300, hostel: 150, rest: 200, fastfood: 100, repair: 7,  gas: 6, exc: 100, quizReward: 100 },
+    3: { hotel: 0,   hostel: 100, rest: 0,   fastfood: 80,  repair: 5,  gas: 6, exc: 50,  quizReward: 500 } 
+};
