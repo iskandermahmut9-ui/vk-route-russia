2951d04d6f39e4b569deaebe7bb483490f309c80
AlexanderGo23 <biggum@yandex.ru>
2026-03-23 09:51:50 +0300

    3

--- js/main.js
+++ js/main.js
@@ -3,6 +3,5 @@ import { Game } from './core/game.js';
 // Делаем Game глобальным, чтобы HTML-кнопки с onclick="Game.buyItem(...)" продолжали работать!
 window.Game = Game; 
 
-document.addEventListener('DOMContentLoaded', () => {
-    Game.init();
-});
+// Вызываем инициализацию напрямую, без addEventListener
+Game.init();
